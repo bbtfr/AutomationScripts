d259ef0d711b8379ceacad084aef1ed4c8bdaeba
Theo Li <bbtfrr@gmail.com>
2016-09-30 13:56:13 +0800

    update script

--- viewclient.py
+++ viewclient.py
@@ -2,8 +2,7 @@
 # encoding: utf-8
 
 from com.dtmilano.android.viewclient import ViewClient
-import time
-import os
+import os, sys, time
 
 device, serialno = ViewClient.connectToDeviceOrExit(verbose=False)
 print "Device initialized... SerialNo: " + serialno
@@ -73,6 +72,19 @@ def compareColor(color1, color2, threshold = 0):
 
 
 
+
+
+
+
+DIALOG_POINT = (345, 200)
+DIALOG_COLOR = (72, 55, 39, 255)
+
+BATTLE_POINT = (1910, 100)
+BATTLE_COLOR = (152, 110, 53, 255)
+
+STAGE_POINT = (1910, 75)
+STAGE_COLOR = (120, 71, 29, 255)
+
 def closeDialog(image):
   if compareColor(getColor((725, 625), image), (221, 105, 81, 255)):
     touch((725, 625), "关闭对话框")
@@ -88,19 +100,19 @@ def enterType1Step1(section):
 
   point = (1800, 300 + 150 * ((section - 1) % 4))
   touch(point, "章节" + str(section), 1)
-  for image in enumerateEnsureColor((345, 200), (72, 55, 38, 255), "对话框"):
+  for image in enumerateEnsureColor(DIALOG_POINT, DIALOG_COLOR, "对话框"):
     closeDialog(image)
   enterType1Step2()
 
 def enterType1Step2():
   touch((700, 350), "困难", 1)
   touch((1450, 800), "探索", 5)
-  for image in enumerateEnsureColor((1910, 75), (119, 70, 29, 255), "进入章节"):
+  for image in enumerateEnsureColor(STAGE_POINT, STAGE_COLOR, "进入章节"):
     closeDialog(image)
 
 def battleType1(section):
   times = 0
-  for image in enumerateEnsureColor((1910, 100), (152, 110, 53, 255), "进入战场", 100):
+  for image in enumerateEnsureColor(BATTLE_POINT, BATTLE_COLOR, "进入战场", 100):
     times += 1
 
     closeDialog(image)
@@ -114,10 +126,10 @@ def battleType1(section):
     if compareColor(getColor((345, 200), image), (72, 55, 38, 255)):
       enterType1Step2()
 
-    if compareColor(getColor((1500, 970), image), (45, 33, 26, 255), 100):
+    if not compareColor(getColor((1500, 970), image), (69, 86, 214, 255)):
       touch((1500, 970), "自动准备")
 
-    point = findColor((244, 225, 215, 255), region, image) # 首领
+    point = findColor((239, 16, 16, 255), region, image) # 首领
     if point:
       touch(point, "选择首领")
       continue
@@ -130,7 +142,7 @@ def battleType1(section):
     point = findColor((255, 244, 211, 255), region, image) # 奖励
     if point:
       touch(point, "收取奖励", 2)
-      touch((1910, 75), "收取奖励")
+      touch(STAGE_POINT, "收取奖励")
       continue
 
     if times % 10 < 5:
@@ -138,9 +150,9 @@ def battleType1(section):
     else:
       drag((200, 500), (800, 500), "寻找怪物")
 
-  for image in enumerateEnsureColor((1910, 75), (119, 70, 29, 255), "离开战场"):
+  for image in enumerateEnsureColor(STAGE_POINT, STAGE_COLOR, "离开战场"):
     closeDialog(image)
-    touch((1910, 75), "离开战场")
+    touch(STAGE_POINT, "离开战场")
 
 def startType1(section):
   if compareColor(getColor((1910, 200)), (111, 70, 29, 255)):
@@ -149,7 +161,7 @@ def startType1(section):
     battleType1(section)
 
 def battleType2(point):
-  for image in enumerateEnsureColor((1910, 100), (152, 110, 53, 255), "进入战场", 100):
+  for image in enumerateEnsureColor(BATTLE_POINT, BATTLE_COLOR, "进入战场", 100):
     closeDialog(image)
 
     if compareColor(getColor((1726, 820), image), (255, 242, 206, 255), 100):
@@ -183,25 +195,58 @@ def startType3():
     touch((1750, 850), "准备")
     touch((1210, 650), "继续组队")
 
+
+
+
+
+
 # 第一章
-# startType1(8)
+startType1(10)
 
 # 结界突破
-startType2()
+# startType2()
 
 # 组队
 # startType3()
 
 
+
+
+
+
 # while True:
-#   print getColor((345, 200))
+#   print getColor((1500, 970))
 #   sleep(1)
+
+# print findColor((244, 225, 215, 255), ((1090, 370), (1300, 460)))
 
 # color = getColor((900, 167))
 # print color
 # for point in enumerateFindColor(color):
 #   print point
 
-# print findColor((72, 55, 38, 255), ((0, 100), (1920, 500)))
+# def inRegion(point, region):
+#   return point[0] > region[0][0] and point[1] > region[0][1] and point[0] < region[1][0] and point[1] < region[1][1]
 
-# takeSnapshot().save("snapshots/1.png")
+# def findBestMatchColor(region):
+#   image = takeSnapshot()
+#   pixels = image.load()
+#   colors = {}
+#   for y in xrange(region[0][1], region[1][1]):
+#     for x in xrange(region[0][0], region[1][0]):
+#       color = pixels[x, y]
+#       if color in colors:
+#         colors[color] += 1
+#       else:
+#         colors[color] = 1
+
+#   sorted_items = sorted(colors.items(), key = lambda item: item[1])[::-1]
+
+#   for item in sorted_items:
+#     point = findColor(item[0], image = image)
+#     if inRegion(point, region):
+#       print item
+
+# findBestMatchColor(((1090, 370), (1300, 460)))
+
+# takeSnapshot().save("snapshots/2.png")
